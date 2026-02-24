26f6036d15f4f1e15809828b00252fc41b1f1807
enigmatic525 <enigmatic29alt@gmail.com>
2026-02-24 15:35:09 -0800

    Update Service Worker cache version to flush old styles

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'calorie-tracker-v1';
+const CACHE_NAME = 'calorie-tracker-v2';
 const urlsToCache = [
     './',
     './index.html',
@@ -9,11 +9,30 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+    // Force the waiting service worker to become the active service worker.
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => {
                 return cache.addAll(urlsToCache);
             })
+    );
+});
+
+self.addEventListener('activate', event => {
+    // Delete all older overlapping caches
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(() => {
+            self.clients.claim(); // Take control immediately
+        })
     );
 });
 
